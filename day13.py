76945feca32f16a6f9e7ed6faf2ac2a1a1177df8
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-08-22 00:27:12 +0530

    add reversing linkedlist

--- day13.py
+++ day13.py
@@ -9,6 +9,24 @@ class Node:
             curr=curr.next
         print("NULL")
 
+    def reversing(self,curr):
+            prev=None
+            while curr!=None:
+               next_node=curr.next
+               curr.next=prev
+               prev=curr
+               curr=next_node
+               head2=curr
+            return head2
+
+    def compersion(self,head1,head2):
+         curr=head1
+         curr2=head2
+         while curr!=None and curr2!=None:
+                if curr.data!=curr2.data:
+                    return "NO"
+         return "YES"
+
 
 node1=Node(1)            
 node2=Node(2)
@@ -28,6 +46,13 @@ head=node1
 
 print("original single linkedlist:")
 head.traveral(head)
+
+hd=head.reversing(head)
+
+print("GIven single linkedlist is palindrome :")
+ans=head.compersion(head,hd)
+print(ans)
+
 
 
 
