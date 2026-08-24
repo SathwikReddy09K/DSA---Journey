448c43f0243b8c3d977979f366f98e5c5e79d427
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-08-24 22:39:42 +0530

    p16 solution

---
+++ prb16.py
@@ -0,0 +1,57 @@
+'''Reversing only specificed nodes'''
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+    def traveral(self,curr):
+        while curr!=None:
+            print(curr.data,"-<",end=" ")
+            curr=curr.next
+        print("NULL")
+
+
+    def finding_nodes(self,head,m,n):
+        curr=head
+        count=0
+        while curr != None :
+            count+=1
+            if count==m:
+                temp1=curr.data
+                new_node=curr
+
+            elif count==n:
+                temp2=curr.data
+                curr.data=temp1
+                new_node.data=temp2
+            
+            curr=curr.next    
+        return head            
+
+
+
+node1=Node(10)            
+node2=Node(20)
+node3=Node(30)
+node4=Node(40)
+node5=Node(50)
+node6=Node(60)
+
+node1.next=node2
+node2.next=node3
+node3.next=node4
+node4.next=node5
+node5.next=node6
+node6.next=None
+
+head=node1
+
+print("Original linked list:")
+head.traveral(head)
+
+m=int(input("Enter m value:"))
+n=int(input("Enter n value:"))
+new_head=head.finding_nodes(head,m,n)
+head.traveral(new_head)
+
