96b798bd57e6eff90b88ebbbf79f2beb22f9d4bb
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-09-02 00:00:11 +0530

    add copy of original linked list

--- prb13.py
+++ prb13.py
@@ -1,3 +1,4 @@
+'''checking given linked list is palindrome'''
 class Node:
     def __init__(self,data):
         self.data=data
@@ -21,16 +22,18 @@ class Node:
     
             return head2
 
-    def compersion(self,head1,head2):
-        curr1=head1
-        curr2=head2
-        while curr1 and curr2:
-            if curr1.data!=curr2.data:
-                return False
-            curr1=curr1.next
-            curr2=curr2.next
-        return True
 
+    def compersion(self,head1,head2):
+            curr1=head1
+            curr2=head2
+            while curr1  and curr2 :
+                if curr1.data !=curr2.data:
+                    return False
+                
+                curr1=curr1.next
+                curr2=curr2.next
+    
+            return True 
 
 node1=Node(1)            
 node2=Node(2)
@@ -46,12 +49,28 @@ node4.next=node5
 node5.next=node6
 node6.next=None
 
+copy1=Node(1)            
+copy2=Node(2)
+copy3=Node(3)
+copy4=Node(3)
+copy5=Node(2)
+copy6=Node(1)
+
+copy1.next=copy2
+copy2.next=copy3
+copy3.next=copy4
+copy4.next=copy5
+copy5.next=copy6
+copy6.next=None
+
 head=node1
+head2=copy1
 
 print("original single linkedlist:")
 head.traveral(head)
 
-head2=head.reversing(head)
+head2=head2.reversing(head2)
+head2.traveral(head2)
 
 
 print("Given single linkedlist is palindrome :")
