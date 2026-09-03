1f8eac43846c8ffba419ff939d8031aee2db3fda
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-09-03 19:03:31 +0530

    added counting funtion

--- prb19.py
+++ prb19.py
@@ -1,3 +1,4 @@
+'''Reversing kth node from 1st with kth node from last'''
 class Node:
     def __init__(self,data):
         self.data=data
@@ -9,22 +10,34 @@ class Node:
             curr=curr.next
         print("NULL")
 
-    def move_upto_kplace(self,head,k):
-            curr=head
-            count=0
-            while curr!=None and count < k:
-                curr=curr.next
-                count+=1
-            fast=curr
-            return fast   
+    def counting_nodes(self,head):
+        count=0
+        curr=head
+        while curr:
+            count+=1
+            curr=curr.next
+        return count    
+ 
         
-    def swapping_kth_node(self,head,fast):
-            slow=head
-            while fast!=None:
-                slow=slow.next
-                fast=fast.next
-            slow.data,fast.data=fast.data,slow.data
-            return head    
+    def swapping_kth_node(self,head,kth_first,kth_last):
+        count=1
+        curr=head
+        while curr:
+            if kth_first==count:
+                firstK_node=curr
+                count+=1
+                curr=curr.next
+
+            if kth_last==count:
+                curr.data,firstK_node.data=firstK_node.data,curr.data
+                
+                
+            
+            curr=curr.next
+            count+=1
+
+        return head  
+            
          
 
 node1=Node(10)
@@ -42,13 +55,16 @@ node5.next=None
 head=node1
 
 print("Original single linkedlist:")
-node1.traveral(head)
+head.traveral(head)
+
+count=head.counting_nodes(head)
+print(count)
 
 k=int(input("Enter k value:"))
 
-fast=head.move_upto_kplace(head,k)
+lastKth_node=count-k+1
 
-print("kth node value from the single linkedlist:",end=" ")
-head.swapping_kth_node(head,fast)
 
-head.traveral(head)
+new_head=head.swapping_kth_node(head,k,lastKth_node)
+
+new_head.traveral(new_head)
