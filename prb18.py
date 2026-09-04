562cc1e323fdb1abeb950b300eb38c8a14d522bc
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-09-04 13:41:25 +0530

    odd-even linked list

--- prb18.py
+++ prb18.py
@@ -3,44 +3,55 @@ class Node:
         self.data=data
         self.next=None
 
-    def traveral(self,curr):
+    def traveral(self,head):
+        curr=head
         while curr!=None:
             print(curr.data,"-<",end=" ")
             curr=curr.next
         print("NULL")
 
-    def compare_odd_even(self,head):
-
-        odd_tail=even_tail=head
-        odd_head=even_head=None
+    def divide(self,head):
+        ohead=ehead=None
         curr=head
 
-        while(curr !=None):
-
-            if curr.data % 2 !=0:
-
-                if even_head!=None:
-                    prev_even,prev_odd=prev_odd,prev_even
-                curr_odd=curr_odd.next
+        while curr != None:
+            next_node=curr.next
+            curr.next=None
             
+            if curr.data % 2 == 0 :
 
+                if ehead == None :
+                    ehead=curr
+                    etail=ehead
+
+                etail.next=curr
+                etail=etail.next
             else:
-                prev_even=curr_even.data
-                curr_even=curr_even.next
+                if ohead == None :
+                    ohead=curr
+                    otail=ohead
 
-        return head       
+                otail.next=curr
+                otail=otail.next
+                    
+            curr=next_node
+        otail.next=ehead
+        etail.next= None
+    
+        return ohead
 
-head=Node(17)
+      
+head=Node(16)
 head.next=Node(15)
 head.next.next=Node(8)
 head.next.next.next=Node(7)
 head.next.next.next.next=Node(2)
-head.next.next.next.next.next=Node(4)
+head.next.next.next.next.next=Node(1)
 head.next.next.next.next.next.next=Node(6)
 
 print("single linked list:")
 head.traveral(head)
-
-ans=head.compare_odd_even(head)
+ans=head.divide(head)
+print("Odd--even single linked list:")
 head.traveral(ans)
 
