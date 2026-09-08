d107b0b0e7df37201d11b67faebc20931c4bfc82
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-09-08 15:59:00 +0530

    reversing string solution

---
+++ prb25.py
@@ -0,0 +1,16 @@
+class solution:
+
+    def reverseing_string(self,str):
+        n=len(str)
+        for i in range(int(n/2)):
+            str[i],str[n-i-1]=str[n-i-1],str[i]
+        return str
+
+s1=solution()
+str=input("Enter string:")
+
+str=list(str)
+print("Original string in list:",str)
+
+ans=s1.reverseing_string(str)      
+print("After reversing string:",ans) 
