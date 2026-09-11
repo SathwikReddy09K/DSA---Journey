d8f45761e7a214587d01ddeb55530ea47eb6e8c0
SathwikReddy09k <Sathwikreddykethidi09@gmail.com>
2026-09-11 17:40:49 +0530

    extra if statement add

--- prb28.py
+++ prb28.py
@@ -1,22 +1,23 @@
-'''palindrome two after deleting one character'''
+'''palindrome II after deleting one character'''
 class string:
     def ispalindrome(self,str):
         n=len(str)
         count=0
         for i in range(int(n/2)):
             if str[i]!=str[n-i-1]:
-                count+=1
-
-        if count>1:
-            return 0
-        else:       
-            return 1
+                if len(str)<=3:
+                    return 0
+                count=+1
+                if count>1:
+                    return 0
+               
+        return 1
 
 s=string()
 str=input("Enter string:")
 ans=s.ispalindrome(str)
 
 if ans:
-    print(f"{str} is a Palindrome")
+    print(f"{str} is a Palindrome after deleting one character")
 else:
-    print(f"{str} is not palindrome")      
+    print(f"{str} is not palindrome after deleting one character")      
