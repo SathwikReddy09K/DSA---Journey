5dc4d29ae9b26fb99a28b13627bdae66069c96b8
SathwikReddy09K <sathwikreddykethidi09@gmail.com>
2026-09-12 22:59:46 +0530

    prb29 solution

--- prb29.py
+++ prb29.py
@@ -1,12 +1,17 @@
+'''longest palindrom substring'''
+
 class string:
     def longest_palindrom(self,str):
         n=len(str)
+        str1=str
         for i in range(int(n/2)):
             if str[i]!=str[n-i-1]:
-                str=str[i+1:n-i-1]
-        return str
+                str1=str[i+1:n-i-1]
+        if len(str1)==0:
+            str1=None        
+        return str1
 
 s=string()
 str=input("Enter string:")
 ans=s.longest_palindrom(str)
-print(ans)
+print("longest palindrom substring is:",ans)
