9c86454ea3dd2f09d8825fc019f7102bce71c191
SathwikReddy09K <Sathwikreddykethidi09@gmail.com>
2026-09-23 17:52:24 +0530

    solution

--- prb33.py
+++ prb33.py
@@ -7,15 +7,15 @@ class solution:
         for i in range(len(str)):
             comp=str[i]
             if comp in dict :
-                if dict[comp] >k:
-                    dict.clear()
-                    prv_length=0
-                else:
                     prv_length+=1    
             else: 
-                dict[comp]=j
-                j+=1
-                prv_length+=1
+                if  j > k:
+                    dict.clear()
+
+                else:    
+                    dict[comp]=j
+                    j+=1
+                    prv_length+=1
             if prv_length > length:
                 length=prv_length
         return length        
@@ -25,4 +25,4 @@ s1=solution()
 str=input("Enter string:")
 k=int(input("Enter size of k: "))
 ans=s1.longest_ksubstring(str,k)  
-print("Longest substring without repeating characters:",ans)        
+print(f"Longest substring  {k} unique :",ans)        
